ee02dad3f2634f529ad2f062517857edaabb353c
MateusHBR <mateusmorais.r@gmail.com>
2020-06-22 16:58:12 -0300

    criando modelo do item de compras.

---
+++ lib/app/shared/models/item_model.dart
@@ -0,0 +1,31 @@
+class Item {
+  String title;
+  String subtitle;
+  double price;
+  bool isFavorite;
+  bool inCart;
+
+  Item({
+    this.title,
+    this.subtitle,
+    this.price,
+    this.isFavorite = false,
+    this.inCart = false,
+  });
+
+  copyWith({
+    String title,
+    String subtitle,
+    double price,
+    bool isFavorite,
+    bool inCart,
+  }) {
+    return Item(
+      title: title ?? this.title,
+      subtitle: subtitle ?? this.subtitle,
+      price: price ?? this.price,
+      isFavorite: isFavorite ?? this.isFavorite,
+      inCart: inCart ?? this.inCart,
+    );
+  }
+}
