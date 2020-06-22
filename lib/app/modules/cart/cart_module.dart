6e356b249bdc9180a1b5338b53a99c98bbddd2d7
MateusHBR <mateusmorais.r@gmail.com>
2020-06-22 16:55:01 -0300

    criando modulo de carrinho de compras

---
+++ lib/app/modules/cart/cart_module.dart
@@ -0,0 +1,18 @@
+import 'cart_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:dio/dio.dart';
+import 'cart_page.dart';
+
+class CartModule extends ChildModule {
+  @override
+  List<Bind> get binds => [
+        Bind((i) => CartController()),
+      ];
+
+  @override
+  List<Router> get routers => [
+        Router(Modular.initialRoute, child: (_, args) => CartPage()),
+      ];
+
+  static Inject get to => Inject<CartModule>.of();
+}
