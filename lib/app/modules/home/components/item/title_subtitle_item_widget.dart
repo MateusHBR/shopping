21ff837fb6c5054b2e9efb8bbad03f050849815f
MateusHBR <mateusmorais.r@gmail.com>
2020-06-23 11:09:55 -0300

    widget reutilizavel de titulo e subtitulo do item

--- lib/app/modules/home/components/item/title_subtitle_item_widget.dart
+++ lib/app/modules/home/components/item/title_subtitle_item_widget.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 
 class TitleSubtitleItemWidget extends StatelessWidget {
+  final String title;
+  final String substitle;
+
+  TitleSubtitleItemWidget({
+    @required this.title,
+    @required this.substitle,
+  });
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         Text(
-          'Sneakers',
+          '$title',
           style: TextStyle(
             color: Colors.white,
             fontSize: 24,
           ),
         ),
         Text(
-          'Nike',
+          '$substitle',
           style: TextStyle(
             color: Colors.white,
             fontSize: 16,
