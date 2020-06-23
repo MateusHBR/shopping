2f86bcc9ba6ad1c395f72fc5b9d672635d504e09
MateusHBR <mateusmorais.r@gmail.com>
2020-06-23 11:11:42 -0300

    criando lista de dados

---
+++ lib/app/shared/data/dummy_data.dart
@@ -0,0 +1,25 @@
+import '../models/item_model.dart';
+
+List<ItemModel> dumyProducts = [
+  ItemModel(
+    id: 1,
+    price: 100.99,
+    title: 'Tenis da Nike 1',
+    subtitle: 'Nike',
+    image: 'assets/images/nike1.png',
+  ),
+  ItemModel(
+    id: 2,
+    price: 100.99,
+    title: 'Tenis da Nike 2',
+    subtitle: 'Nike',
+    image: 'assets/images/nike2.jpg',
+  ),
+  ItemModel(
+    id: 3,
+    price: 100.99,
+    title: 'Tenis da Nike 3',
+    subtitle: 'Nike',
+    image: 'assets/images/nike4.jpg',
+  ),
+];
