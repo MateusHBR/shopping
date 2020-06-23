8fd06309463f10b0d0754f8240c465203fbabbea
MateusHBR <mateusmorais.r@gmail.com>
2020-06-23 11:08:58 -0300

    criando widget totalmente responsivo

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:my_shopping/app/modules/home/components/category/category_widget.dart';
 import 'package:my_shopping/app/modules/home/components/item/item_widget.dart';
+import 'package:my_shopping/app/shared/models/item_model.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -55,28 +57,38 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
       body: Column(
         children: <Widget>[
           CategoryWidget(),
-          Container(
-            padding: EdgeInsets.only(top: 16),
-            height: size.height * 0.82,
-            width: double.infinity,
-            child: ListView(
-              padding: EdgeInsets.only(top: 10),
-              children: <Widget>[
-                ItemWidget(
-                  color: Colors.red,
-                  image: 'assets/images/nike1.png',
-                ),
-                ItemWidget(
-                  color: Colors.blue,
-                  image: 'assets/images/nike2.jpg',
-                ),
-                ItemWidget(
-                  color: Colors.grey[350],
-                  image: 'assets/images/nike4.jpg',
-                ),
-              ],
+          Expanded(
+            child: Container(
+              child: LayoutBuilder(
+                builder: (_, constraints) {
+                  return Container(
+                    padding: EdgeInsets.only(top: 16),
+                    height: constraints.maxHeight,
+                    width: constraints.maxWidth,
+                    child: Observer(
+                      builder: (_) {
+                        return ListView.builder(
+                          padding: EdgeInsets.only(top: 10),
+                          itemCount: controller.productList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            ItemModel item = controller.productList[index];
+                            return InkWell(
+                              onTap: () => Modular.to
+                                  .pushNamed('/item', arguments: item),
+                              child: ItemWidget(
+                                item: item,
+                                index: index,
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
+          )
         ],
       ),
     );
