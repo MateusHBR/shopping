12f6cb3448ea91da468a00dfeac52fb048935c39
MateusHBR <mateusmorais.r@gmail.com>
2020-06-23 11:06:20 -0300

    arquivos gerados

--- lib/app/modules/home/home_controller.g.dart
+++ lib/app/modules/home/home_controller.g.dart
@@ -9,39 +9,34 @@ part of 'home_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$HomeController on _HomeControllerBase, Store {
-  final _$valueAtom = Atom(name: '_HomeControllerBase.value');
+  Computed<List<ItemModel>> _$favoritesComputed;
 
   @override
-  int get value {
-    _$valueAtom.reportRead();
-    return super.value;
+  List<ItemModel> get favorites =>
+      (_$favoritesComputed ??= Computed<List<ItemModel>>(() => super.favorites,
+              name: '_HomeControllerBase.favorites'))
+          .value;
+
+  final _$productListAtom = Atom(name: '_HomeControllerBase.productList');
+
+  @override
+  ObservableList<ItemModel> get productList {
+    _$productListAtom.reportRead();
+    return super.productList;
   }
 
   @override
-  set value(int value) {
-    _$valueAtom.reportWrite(value, super.value, () {
-      super.value = value;
+  set productList(ObservableList<ItemModel> value) {
+    _$productListAtom.reportWrite(value, super.productList, () {
+      super.productList = value;
     });
-  }
-
-  final _$_HomeControllerBaseActionController =
-      ActionController(name: '_HomeControllerBase');
-
-  @override
-  void increment() {
-    final _$actionInfo = _$_HomeControllerBaseActionController.startAction(
-        name: '_HomeControllerBase.increment');
-    try {
-      return super.increment();
-    } finally {
-      _$_HomeControllerBaseActionController.endAction(_$actionInfo);
-    }
   }
 
   @override
   String toString() {
     return '''
-value: ${value}
+productList: ${productList},
+favorites: ${favorites}
     ''';
   }
 }
