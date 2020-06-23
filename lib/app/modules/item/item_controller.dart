64a7693bf5f9873cc3829d9ea805eb0b94f1c7fe
MateusHBR <mateusmorais.r@gmail.com>
2020-06-23 16:22:20 -0300

    removendo codigo nao utilizado

--- lib/app/modules/item/item_controller.dart
+++ lib/app/modules/item/item_controller.dart
@@ -4,12 +4,4 @@ part 'item_controller.g.dart';
 
 class ItemController = _ItemControllerBase with _$ItemController;
 
-abstract class _ItemControllerBase with Store {
-  @observable
-  int value = 0;
-
-  @action
-  void increment() {
-    value++;
-  }
-}
+abstract class _ItemControllerBase with Store {}
