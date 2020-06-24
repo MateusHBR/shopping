693c56021d71f53880da31220c6c4edf7804adfe
MateusHBR <mateusmorais.r@gmail.com>
2020-06-24 08:54:34 -0300

    adicionando funcionalidade as categorias.

--- lib/app/modules/home/components/category/item_category_widget.dart
+++ lib/app/modules/home/components/category/item_category_widget.dart
@@ -3,30 +3,35 @@ import 'package:flutter/material.dart';
 class ItemCategoryWidget extends StatelessWidget {
   final String title;
   final bool selected;
+  final Function function;
 
   const ItemCategoryWidget({
     @required this.title,
     this.selected = false,
+    @required this.function,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      child: AspectRatio(
-        aspectRatio: 2 / 1,
-        child: Container(
-          decoration: BoxDecoration(
-            color: selected
-                ? Colors.grey[350]
-                : Theme.of(context).scaffoldBackgroundColor,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          alignment: Alignment.center,
-          child: Text(
-            '$title',
-            style: TextStyle(
-              fontSize: 16,
+    return InkWell(
+      onTap: function,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: AspectRatio(
+          aspectRatio: 2 / 1,
+          child: Container(
+            decoration: BoxDecoration(
+              color: selected
+                  ? Colors.grey[350]
+                  : Theme.of(context).scaffoldBackgroundColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            alignment: Alignment.center,
+            child: Text(
+              '$title',
+              style: TextStyle(
+                fontSize: 16,
+              ),
             ),
           ),
         ),
