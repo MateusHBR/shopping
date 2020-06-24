90a6172bb311eb32698b8a1e88b0bf52f38afc5a
MateusHBR <mateusmorais.r@gmail.com>
2020-06-24 08:55:01 -0300

    criando funcionalidade ao tamanho do tenis

--- lib/app/modules/item/item_controller.dart
+++ lib/app/modules/item/item_controller.dart
@@ -4,4 +4,13 @@ part 'item_controller.g.dart';
 
 class ItemController = _ItemControllerBase with _$ItemController;
 
-abstract class _ItemControllerBase with Store {}
+abstract class _ItemControllerBase with Store {
+  @observable
+  ObservableList<int> tenisSize = <int>[38, 40, 42, 44].asObservable();
+
+  @observable
+  int selectedSize = 0;
+
+  @action
+  void changeSelected(int i) => selectedSize = i;
+}
--- lib/app/modules/item/item_controller.g.dart
+++ lib/app/modules/item/item_controller.g.dart
@@ -9,10 +9,55 @@ part of 'item_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$ItemController on _ItemControllerBase, Store {
+  final _$tenisSizeAtom = Atom(name: '_ItemControllerBase.tenisSize');
+
+  @override
+  ObservableList<int> get tenisSize {
+    _$tenisSizeAtom.reportRead();
+    return super.tenisSize;
+  }
+
+  @override
+  set tenisSize(ObservableList<int> value) {
+    _$tenisSizeAtom.reportWrite(value, super.tenisSize, () {
+      super.tenisSize = value;
+    });
+  }
+
+  final _$selectedSizeAtom = Atom(name: '_ItemControllerBase.selectedSize');
+
+  @override
+  int get selectedSize {
+    _$selectedSizeAtom.reportRead();
+    return super.selectedSize;
+  }
+
+  @override
+  set selectedSize(int value) {
+    _$selectedSizeAtom.reportWrite(value, super.selectedSize, () {
+      super.selectedSize = value;
+    });
+  }
+
+  final _$_ItemControllerBaseActionController =
+      ActionController(name: '_ItemControllerBase');
+
+  @override
+  void changeSelected(int i) {
+    final _$actionInfo = _$_ItemControllerBaseActionController.startAction(
+        name: '_ItemControllerBase.changeSelected');
+    try {
+      return super.changeSelected(i);
+    } finally {
+      _$_ItemControllerBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+tenisSize: ${tenisSize},
+selectedSize: ${selectedSize}
     ''';
   }
 }
